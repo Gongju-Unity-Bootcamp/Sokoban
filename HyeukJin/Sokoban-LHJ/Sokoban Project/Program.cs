2932de4b4e7b93bf4f2d892bb9ee8ea3bbb0d069
HyeukJin <lytmmr@gmail.com>
2023-11-02 15:33:28 +0900

    LHJ sokoban game

--- HyeukJin/Sokoban-LHJ/Sokoban Project/Program.cs
+++ HyeukJin/Sokoban-LHJ/Sokoban Project/Program.cs
@@ -18,15 +18,12 @@ namespace Sokoban_Project
    
         static void Main(string[] args)
         {
-         
-
             Console.ResetColor();
             Console.CursorVisible = false;
             Console.Title = "LHJ Sokoban";
             Console.BackgroundColor = ConsoleColor.Green;
             Console.ForegroundColor = ConsoleColor.Red;
           
-
             // 플레이어 좌표
             int playerX = 12;
             int playerY = 15;
@@ -34,8 +31,15 @@ namespace Sokoban_Project
             int boxX = 15;
             int boxY = 15;
 
+            int box2X = 13;
+            int box2Y = 13;
+
             int CircleX = 20;
             int CircleY = 20;
+
+            int Circle2X = 17;
+            int Circle2Y = 17;
+
             Direction playerDirection = Direction.None;
 
             // 콘솔크기
@@ -47,25 +51,24 @@ namespace Sokoban_Project
 
             //int playerDirection = 0; // 0 : None, 1 : Left, 2 : Right, 3 : Up, 4 : Down
 
-
             int wallX = 10;
             int wallY = 10;
-            
+            int wallXMax = 25;
+            int wallYMax = 25;
 
             while (true)
             {
                 // ----------------------------- Render ------------------------------
-
                 Console.Clear();
        
-                for(int i = wallX; i< 25; i++)
+                for(int i = wallX; i< wallXMax; i++)
                 {
                     Console.SetCursorPosition(i, wallY);
                     Console.Write("―");
                     Console.SetCursorPosition(i, 25);
                     Console.Write("―");
                 }
-                for (int i = wallY; i < 25; i++)
+                for (int i = wallY; i < wallYMax; i++)
                 {
                     Console.SetCursorPosition(wallX, i);
                     Console.Write("|");
@@ -73,20 +76,22 @@ namespace Sokoban_Project
                     Console.Write("|");
                 }
 
-
                 //이전 화면을 지운다.
                 Console.CursorVisible = false;    //커서를 숨긴다.
                                                   //플레이어를 출력한다.
                 Console.SetCursorPosition(CircleX, CircleY);
                 Console.Write("O");
+                Console.SetCursorPosition(Circle2X, Circle2Y);
+                Console.Write("O");
+
                 if (boxX == wallX)
                 {
                     boxX = wallX + 1;
                     playerX += 1;
                 }
-                if (boxX == 25)
+                if (boxX == wallXMax)
                 {
-                    boxX = 24;
+                    boxX = wallXMax - 1;
                     playerX -= 1;
                 }
                 if (boxY == wallY)
@@ -94,27 +99,70 @@ namespace Sokoban_Project
                     boxY = wallY + 1;
                     playerY += 1;
                 }
-                if( boxY == 25)
+                if( boxY == wallYMax)
                 {
-                    boxY = 24;
+                    boxY = wallYMax - 1;
                     playerY -= 1;
                 }
-               
+                if (box2X == wallX)
+                {
+                    box2X = wallX + 1;
+                    playerX += 1;
+                }
+                if (box2X == wallXMax)
+                {
+                    box2X = wallXMax - 1;
+                    playerX -= 1;
+                }
+                if (box2Y == wallY)
+                {
+                    box2Y = wallY + 1;
+                    playerY += 1;
+                }
+                if (box2Y == wallYMax)
+                {
+                    box2Y = wallYMax - 1;
+                    playerY -= 1;
+                }
+
                 if (boxX == CircleX && boxY == CircleY)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.SetCursorPosition(boxX, boxY);
                     Console.Write("□");
-                    Console.ForegroundColor = ConsoleColor.Red;
+
                 }
-                else
-                {
-                    
+                else if(boxX == Circle2X && boxY == Circle2Y)
+                    {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.SetCursorPosition(boxX, boxY);
                     Console.Write("□");
                 }
-                
-                
+                else
+                {
+                    Console.SetCursorPosition(boxX, boxY);
+                    Console.Write("□");
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                if (box2X == CircleX && box2Y == CircleY)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.SetCursorPosition(box2X, box2Y);
+                    Console.Write("□");
+                }
+                else if (box2X == Circle2X && box2Y == Circle2Y)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.SetCursorPosition(box2X, box2Y);
+                    Console.Write("□");
+                }
+                else
+                { 
+                    Console.SetCursorPosition(box2X, box2Y);
+                    Console.Write("□");
+                }
+                Console.ForegroundColor = ConsoleColor.Red;
+
                 Console.SetCursorPosition(playerX, playerY);
                 Console.Write("A");
 
@@ -133,7 +181,7 @@ namespace Sokoban_Project
                     playerDirection = Direction.Left;
 
     }
-                if (keyInfo.Key == ConsoleKey.D && playerX + 1 < 25)
+                if (keyInfo.Key == ConsoleKey.D && playerX + 1 < wallXMax)
                 {
                     playerX += 1;
                     playerDirection = Direction.Right;
@@ -144,7 +192,7 @@ namespace Sokoban_Project
                     playerDirection = Direction.Up;
 
                 }
-                if (keyInfo.Key == ConsoleKey.S && playerY + 1 < 25)
+                if (keyInfo.Key == ConsoleKey.S && playerY + 1 < wallYMax)
                 {
                     playerY += 1;
                     playerDirection = Direction.Down;
@@ -178,24 +226,71 @@ namespace Sokoban_Project
                     }
 
                 }
+                if (playerX == box2X && playerY == box2Y)
+                {
 
-                //벽의 기능 : 벽의 위치로는 어떤 오브젝트도 위치할 수 없다 => (벽의 좌표) !=(박스의좌표) && !=(플레이어의 좌표)
-                if(wallX == playerX  && wallY == playerY || wallX == boxY && wallY == boxY)
+
+                    switch (playerDirection)
+                    {
+                        case Direction.Left:
+                            box2X -= 1;
+                            playerX = box2X + 1;
+                            break;
+                        case Direction.Right:
+                            box2X += 1;
+                            playerX = box2X - 1;
+                            break;
+                        case Direction.Up:
+                            box2Y -= 1;
+                            playerY = box2Y + 1;
+                            break;
+                        case Direction.Down:
+                            box2Y += 1;
+                            playerY = box2Y - 1;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine("잘못된 방향 데이터입니다.");
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+
+                    //벽의 기능 : 벽의 위치로는 어떤 오브젝트도 위치할 수 없다 => (벽의 좌표) !=(박스의좌표) && !=(플레이어의 좌표)
+                    if (wallX == playerX  && wallY == playerY || wallX == boxY && wallY == boxY)
                 {
                     continue;
                 }
                 if (boxX == CircleX && boxY == CircleY)
                 {
-                    break;
+                    if((box2X == Circle2X && box2Y == Circle2Y))
+                        {
+                        break;
+                        }
+                }
+                else if (boxX == Circle2X && boxY == Circle2Y)
+                {
+                    if ((box2X == CircleX && box2Y == CircleY))
+                    {
+                        break;
+                    }
                 }
 
+                if (keyInfo.Key == ConsoleKey.Spacebar)
+                {
+                    playerX = 12;
+                    playerY = 15;
 
+                    boxX = 15;
+                    boxY = 15;
+                    box2X = 13;
+                    box2Y = 13;
+                }
 
             }
 
             Console.Clear();
             Console.Write("축하합니다. 소코반을 깼습니다.");
-
 
         }
     }
