3f5cdc3d7e0a6202ea7227a87095fa290989f7dd
morloc <vvv5812@gmail.com>
2023-11-02 09:14:21 +0900

    소코반
    소코반

--- cho/Sokoban-cho/good/Program.cs
+++ cho/Sokoban-cho/good/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace good
+﻿using System;
+
+namespace Sokoban
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.ResetColor();
+            Console.CursorVisible = false;
+            Console.Title = ("j");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            int PlayerX = 10;
+            int PlayerY = 10;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.CursorVisible = false;
+
+                Console.SetCursorPosition(PlayerX, PlayerY);
+                Console.Write("j");
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                ConsoleKey key = keyInfo.Key;
+                if (key == ConsoleKey.RightArrow && PlayerX +1<Console.BufferWidth)
+                {
+                    PlayerX += 1;
+                }
+                if (key == ConsoleKey.LeftArrow && PlayerX>0)
+                {
+                    PlayerX -= 1;
+                }
+                if (key == ConsoleKey.UpArrow && PlayerY>0)
+                {
+                    PlayerY -= 1;
+                }
+                if (key == ConsoleKey.DownArrow && PlayerY +1 <Console.BufferHeight)
+                {
+                    PlayerY += 1;
+                }
+            }
         }
     }
 }
