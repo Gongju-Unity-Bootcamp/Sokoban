f4e7a416cde8ed135ade3453e6c243eacd9e6bce
2gangmin <gm010815@gmail.com>
2023-11-02 12:52:06 +0900

    Modify playermove and Create new box

--- Leegm/Sokoban-leegm/Bible/Program.cs
+++ Leegm/Sokoban-leegm/Bible/Program.cs
@@ -1,43 +1,172 @@
 ﻿using System;
-
+using System.Net.Sockets;
+enum Direction
+{
+    None,
+    Left,
+    Right,
+    Up,
+    Down
+ }
 
 namespace Bible
 {
     internal class Program
     {
+
         static void Main()
         {
-            Console.ResetColor();                               // 컬러를 초기화 한다.
-            Console.CursorVisible = false;                      // 커서를 숨긴다.
-            Console.Title = "My Sokonan";                       // 타이틀을 설정한다.
-            Console.BackgroundColor = ConsoleColor.DarkBlue;    // 배경색을 설정한다.
-            Console.ForegroundColor = ConsoleColor.Gray;       // 글꼴색을 설정한다.
-            Console.Clear();                                    // 콘솔 창에 출력된 내용을 모두 지운다.
+            Console.ResetColor();
+            Console.CursorVisible = false;
+            Console.Title = "My Sokonan";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Clear();                                    
 
-            // 플레이어 좌표
+            // 플레이어
             int playerX = 10;
             int playerY = 5;
+            // int speed = 1;
 
-            while (true) 
+            // 박스
+            int boxX = 7;
+            int boxY = 7;
+
+            // 골~
+            int goalX = 30;
+            int goalY = 30;
+
+            Direction playerDirection = Direction.None;
+
+            while (true)
             {
-                // ------------------ Render ---------------------
+                // ---------------------- Render ----------------------
                 Console.Clear();
                 Console.CursorVisible = false;
 
                 // 플레이어를 출력한다.
                 Console.SetCursorPosition(playerX, playerY);
-                Console.Write("◆");
+                Console.Write("★");
 
-                // ------------------ ProcessInput ---------------
+                // 박스를 출력한다.
+                Console.SetCursorPosition(boxX, boxY);
+                Console.Write("□");
+
+                // ---------------------- ProcessInput ----------------------
                 // 유저로부터 입력을 받는다.
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
 
 
-                // ------------------ Update ---------------------
-                if (keyInfo.Key == ConsoleKey.Escape)           // Esc 누를시 종료
+                // ---------------------- Update ----------------------
+                int newPlayerX = playerX;
+                int newPlayerY = playerY;
+
+                int newBoxX = boxX;
+                int newBoxY = boxY;
+
+
+                // 플레이어 이동
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    newPlayerX -= 1;
+                    playerDirection = Direction.Left;
+                }
+                if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    newPlayerX += 1;
+                    playerDirection = Direction.Right;
+                }
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    newPlayerY -= 1;
+                    playerDirection = Direction.Up;
+                }
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    newPlayerY += 1;
+                    playerDirection = Direction.Down;
+                }
+
+                if (keyInfo.Key == ConsoleKey.Escape)
                 {
                     Environment.Exit(0);
                 }
+                // }
+
+                // 플레이어와 박스가 충돌했을 때, 박스는 플레이어가 이동한 방향으로 한 칸 이동한다.
+                if ( newPlayerX == newBoxX  && newPlayerY == newBoxY ) 
+                {
+                    switch (playerDirection)
+                    {
+                        case Direction.Left:
+                            newBoxX -= 1;
+                            break;
+                        case Direction.Right:
+                            newBoxX += 1;
+                            break;
+                        case Direction.Up:
+                            newBoxY -= 1;
+                            break;
+                        case Direction.Down:
+                            newBoxY += 1;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine($"잘못된 방향 데이터입니다. 실제 데이터는 {playerDirection}");
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+
+                // 박스 유효성
+                if (0 <= newBoxX && newBoxX < Console.BufferWidth)
+                {
+                    boxX = newBoxX;
+                }
+                else
+                {
+                    switch(playerDirection)
+                    {
+                        case Direction.Left:
+                            newPlayerX += 1;
+                            break;
+                        case Direction.Right:
+                            newPlayerX -= 1;
+                            break;
+                    }
+                }
+                if (0 <= newBoxY && newBoxY < Console.BufferHeight)
+                {
+                    boxY = newBoxY;
+                }
+                else
+                {
+                    switch (playerDirection)
+                    {
+                        case Direction.Up:
+                            newPlayerY += 1;
+                            break;
+                        case Direction.Down:
+                            newPlayerY -= 1;
+                            break;
+
+                    }
+                }
+
+                //플레이어 유효성
+                if (0 <= newPlayerX && newPlayerX < Console.BufferWidth)
+                {
+                    playerX = newPlayerX;
+                }
+                if (0 <= newPlayerY && newPlayerY < Console.BufferHeight)
+                {
+                    playerY = newPlayerY;
+                }
+
+
+
+
+                /*
                 if (keyInfo.Modifiers == ConsoleModifiers.Control)              
                 {
                     if (keyInfo.Key == ConsoleKey.LeftArrow && playerX > 1)
@@ -58,24 +187,7 @@ namespace Bible
                     }
                 }
                 else 
-                {
-                    if (keyInfo.Key == ConsoleKey.LeftArrow && playerX > 0)
-                    {
-                        playerX -= 1;
-                    }
-                    if (keyInfo.Key == ConsoleKey.RightArrow && playerX + 1 < Console.BufferWidth)
-                    {
-                        playerX += 1;
-                    }
-                    if (keyInfo.Key == ConsoleKey.UpArrow && playerY > 0)
-                    {
-                        playerY -= 1;
-                    }
-                    if (keyInfo.Key == ConsoleKey.DownArrow && playerY + 1 < Console.BufferHeight)
-                    {
-                        playerY += 1;
-                    }
-                }
+                { */
             }
         }
     }
