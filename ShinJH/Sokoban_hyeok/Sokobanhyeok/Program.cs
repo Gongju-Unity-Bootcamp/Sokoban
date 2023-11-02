1de35705a5b5f0f045c4e061c12a156c68ce18db
ShinJinhyeok96 <shinjinhyeok@gmail.com>
2023-11-02 12:53:14 +0900

    박스 밀기와 열거형

--- ShinJH/Sokoban_hyeok/Sokobanhyeok/Program.cs
+++ ShinJH/Sokoban_hyeok/Sokobanhyeok/Program.cs
@@ -1,11 +1,16 @@
-﻿using System.Data;
+﻿using System.ComponentModel;
+using System.Data;
 using System.Diagnostics;
 
 namespace Sokobanhyeok
 {
+    enum Direction
+    {
+        None, Left, Right, Up, Down
+    }
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.ResetColor();
             Console.CursorVisible = false;
@@ -17,66 +22,114 @@ namespace Sokobanhyeok
             int playerX = 0;
             int playerY = 0;
 
-            int WindowWidth = Console.WindowWidth;
-            int WindowHeight = Console.WindowHeight;
+            int boxX = 1;
+            int boxY = 1;
+
+            // 0 : none, 1 : Left, 2 : Right, 3 : Up, 4 : Down
+            
 
             while (true)
             {
-                // 이전 화면을 지운다. - 깜빡이는 현상의 원인
+                // while문 안에서 Clear, CursorVisible 초기화를 한번 더 실행한다.
                 Console.Clear();
                 Console.CursorVisible = false;
                 
+                // player 생성 : playerX, playerY
                 Console.SetCursorPosition(playerX, playerY);
-                Console.Write("^O^");
-                // Write : 플레이어 모양
+                Console.Write("O");
+
+                // Box 생성 : boxX, boxY
+                Console.SetCursorPosition(boxX, boxY);
+                Console.Write("X");
                 
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                int newPlayerX = playerX;
+                int newPlayerY = playerY;
+
+                // newPlayer와 같은 임시 데이터로 다루어야 한다.
+                int newBoxX = boxX;
+                int newBoxY = boxY;
+
+                Direction playerDirection = Direction.None;
+
                 // 플레이어가 콘솔 화면을 이동한다.
-                if (keyInfo.Key == ConsoleKey.LeftArrow && playerX > 0)
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
                 {
-                    playerX -= 1;
-                    if (keyInfo.Modifiers == ConsoleModifiers.Shift)
-                    {
-                        playerX -= 5;
-                    }
+                    newPlayerX -= 1;
+                    playerDirection = Direction.Left;
                 }
-                if (keyInfo.Key == ConsoleKey.RightArrow && playerX + 3 < WindowWidth)
+                if (keyInfo.Key == ConsoleKey.RightArrow)
                 {
-                    // playerX + 3 : 오른쪽 콘솔 창에서 내가 입력한 ^O^(3)을 나가지 못하게 값을 추가한다.
-                    playerX += 1;
-                    if (keyInfo.Modifiers == ConsoleModifiers.Shift && playerX + 5 < WindowWidth)
-                    {
-                        // ConsoleModifiers.Shift(Alt/Ctrl) : Shift를 입력받아 내부 명령을 수행한다.
-                        playerX += 5;
-                    }
+                    newPlayerX += 1;
+                    playerDirection = Direction.Right;
                 }
-                if (keyInfo.Key == ConsoleKey.UpArrow && playerY > 0)
+                if (keyInfo.Key == ConsoleKey.UpArrow)
                 {
-                    playerY -= 1;
-                    if (keyInfo.Modifiers == ConsoleModifiers.Shift)
-                    {
-                        playerY -= 5;
-                    }
+                    newPlayerY -= 1;
+                    playerDirection = Direction.Up;
                 }
-                if (keyInfo.Key == ConsoleKey.DownArrow && playerY + 1 < WindowHeight)
+                if (keyInfo.Key == ConsoleKey.DownArrow)
                 {
-                    playerY += 1;
-                    if (keyInfo.Modifiers == ConsoleModifiers.Shift && playerY + 5 < WindowHeight)
+                    newPlayerY += 1;
+                    playerDirection = Direction.Down;
+                }
+
+                // Box의 기능 : 플레이어와 충돌했을 때(if), 플레이어가 이동한 방향으로 박스도 한 칸 이동한다.
+                // 충돌 : player, box의 좌표값이 같다.
+                if (newPlayerX == newBoxX && newPlayerY == newBoxY)
+                {
+                    switch (playerDirection)
                     {
-                        playerY += 5;
+                        case Direction.Left:
+                            newBoxX -= 1;
+                            break;
+                        case Direction.Right:
+                            newBoxX += 1;
+                            break;
+                        case Direction.Up:
+                            newBoxY -= 1;
+                            break;
+                        case Direction.Down:
+                            newBoxY += 1;
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine($"잘못된 방향 데이터입니다. 실제 데이터는 {playerDirection}");
+                            Environment.Exit(0);
+                            break;
                     }
                 }
 
-                
-                // 1. 안보고 플레이어 이동 구현
-                // 2. 화면 바깥 오류 수정
-                // 3. 이동과 관련된 기능 하나 이상 구현
+                // newPlayer Console 화면
+                if (0 <= newPlayerX && newPlayerX < Console.BufferWidth)
+                {
+                    playerX = newPlayerX;
+                }
+                if (0 <= newPlayerY && newPlayerY < Console.BufferHeight)
+                {
+                    playerY = newPlayerY;
+                }
 
-                // ConsoleKeyInfo keyInfo = Console.ReadKey();
-                // Console.Clear(); // 이전 화면(입력내용)을 지운다.
-                // Console.WriteLine($"니가 입력한 키 : {keyInfo.Key}");
-                // Console.WriteLine($"니가 입력한 한정자 : {keyInfo.Modifiers}");
-                // Ctrl, Shift, Alt : Modifiers에 저장된다.
+                // newBox Console 화면
+                if (0 <= newBoxX && newBoxX < Console.BufferWidth)
+                {
+                    boxX = newBoxX;
+                }
+                if (0 <= newBoxY && newBoxY < Console.BufferHeight)
+                {
+                    boxY = newBoxY;
+                }
+
+                // Shift 빠른 이동 구현 중..
+                // if (keyInfo.Modifiers == ConsoleModifiers.Shift && keyInfo.Key == ConsoleKey.LeftArrow)
+                // {
+                //     newPlayerX -= 5;
+                // }
+                // if (keyInfo.Modifiers == ConsoleModifiers.Shift && keyInfo.Key == ConsoleKey.RightArrow)
+                // {
+                //     newPlayerX += 5;
+                // }
             }
         }
     }
