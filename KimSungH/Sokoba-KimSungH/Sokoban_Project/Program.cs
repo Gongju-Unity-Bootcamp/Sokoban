0053015305f2ae5138936a56146f1d3de98af3b5
KimCastle07 <rpaehfdl741@gmail.com>
2023-11-02 14:49:38 +0900

    Make First Game

--- KimSungH/Sokoba-KimSungH/Sokoban_Project/Program.cs
+++ KimSungH/Sokoba-KimSungH/Sokoban_Project/Program.cs
@@ -1,5 +1,13 @@
 ﻿namespace Sokoban_Project
 {
+    public enum Direction
+    {
+        None = 0,
+        Left = 1,
+        Right =2,
+        Up = 3,
+        Down =4
+    }
     internal class Program
     {
         static void Main(string[] args)
@@ -112,77 +120,175 @@
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Clear();
 
+            //박스의 기능 : 플레이어와 충돌했을 때, 플레이어가 이동한 박향으로 박스도 한 칸이동.
             int playerX = 30;
             int playerY = 5;
 
-            int objectX = 50;
-            int objectY = 9;
+            //int playerDirection = 0;
+            Direction PlayerDirection = Direction.None;
+            
+            // 0: None, 1 : Left, 2: Right, 3 : Up, 4 : Down
+
+            //int objectX = 50;
+            //int objectY = 9;
 
             Random rand = new Random();
 
+            int boxX = 7;
+            int boxY = 7;
+
+            int wallX = 10;
+            int wallY = 10;
+
+            int goalX = 60;
+            int goalY = 20;
 
 
             while (true)
             {
+                //------------------Reander------------------
                 Console.Clear();
                 Console.CursorVisible = false;
 
                 Console.SetCursorPosition(playerX, playerY);
                 Console.Write("@");
 
-                Console.SetCursorPosition(objectX, objectY);
-                Console.Write("@");
+                //Console.SetCursorPosition(objectX, objectY);
+                //Console.Write("@");
 
+                Console.SetCursorPosition(boxX, boxY);
+                Console.Write('B');
+
+                Console.SetCursorPosition(wallX, wallY);
+                Console.Write('W');
+
+                Console.SetCursorPosition(goalX, goalY);
+                Console.Write("G");
+
+                //----------------ProcessInput-------------
                 ConsoleKeyInfo keyInfo = Console.ReadKey();
                 ConsoleKey Key = keyInfo.Key;
 
-                if (Key == ConsoleKey.LeftArrow && playerX > 0)
-                {
-                    playerX -= 1;
-                }
-                if (Key == ConsoleKey.RightArrow && playerX + 1 < Console.BufferWidth)
-                {
-                    playerX += 1;
-                }
-                if (Key == ConsoleKey.UpArrow && playerY > 0)
-                {
-                    playerY -= 1;
-                }
-                if (Key == ConsoleKey.DownArrow && playerY + 1 < Console.BufferHeight)
-                {
-                    playerY += 1;
-                }
-                if (playerX == objectX && playerY == objectY)
-                {
-                    playerX = rand.Next(0, Console.BufferWidth);
-                    playerY = rand.Next(0, Console.BufferHeight);
+                //----------------Update--------------------
 
-                }
-                //if (Key == ConsoleKey.DownArrow && Key == ConsoleKey.RightArrow)
-                //{
-                //    playerX += 7;
-                //    playerY += 7;
+                PlayerDirection = Direction.None;
 
-                if (keyInfo.Modifiers == ConsoleModifiers.Shift)
+                int newPlayerX = playerX;
+                int newPlayerY = playerY;
+
+                if (Key == ConsoleKey.LeftArrow )
                 {
-                    if (keyInfo.Key == ConsoleKey.LeftArrow && playerX > 4)
+                    newPlayerX -= 1;
+                    PlayerDirection = Direction.Left;
+                }
+                if (Key == ConsoleKey.RightArrow )
+                {
+                    newPlayerX += 1;
+                    PlayerDirection = Direction.Right;
+                }
+                if (Key == ConsoleKey.UpArrow )
+                {
+                    newPlayerY -= 1;
+                    PlayerDirection = Direction.Up;
+                }
+                if (Key == ConsoleKey.DownArrow)
+                {
+                    newPlayerY += 1;
+                    PlayerDirection = Direction.Down;
+                }
+
+                int newBoxX = boxX;
+                int newBoxY = boxY;
+
+                if (newPlayerX == newBoxX && newPlayerY == newBoxY)
+                {
+                    switch (PlayerDirection)
                     {
-                        playerX -= 5;
-                    }
-                    if (Key == ConsoleKey.RightArrow && playerX + 8 < Console.BufferWidth)
-                    {
-                        playerX += 5;
-                    }
-                    if (Key == ConsoleKey.UpArrow && playerY > 4)
-                    {
-                        playerY -= 5;
-                    }
-                    if (Key == ConsoleKey.DownArrow && playerY + 6 < Console.BufferHeight)
-                    {
-                        playerY += 5;
+                        case (Direction)1:
+                            newBoxX -= 1;
+
+                            break;
+                        case (Direction)2:
+                            newBoxX += 1;
+
+                            break;
+                        case (Direction)3:
+                            newBoxY -= 1;
+
+                            break;
+                        case (Direction)4:
+                            newBoxY += 1;
+
+                            break;
+                        default:
+                            Console.Clear();
+                            Console.WriteLine($"잘못된 방향 데이터 입니다. 실제 데이터는 {PlayerDirection}");
+                            Environment.Exit(0);
+                            break;
                     }
                 }
+
+                // 벽의 기능 : 벽의 위치로는 어떤 오브젝트도 위치할 수 없다.
+                // 벽 좌표 != 박스 좌표 && 벽 좌표 != 플레이어 좌표
+
+                if (wallX == newPlayerX && wallY == newPlayerY ||
+                wallX == newBoxX && wallY == newBoxY)
+                {
+                    continue;
+                }
+
+                if (0 <= newPlayerX && newPlayerX < Console.BufferWidth)
+                {
+                    playerX = newPlayerX;
+                }
+                if (0 <= newPlayerY && newPlayerY < Console.BufferHeight)
+                {
+                    playerY = newPlayerY;
+                }
+
+                if (0 <= newBoxX && newBoxX < Console.BufferWidth)
+                {
+                    boxX = newBoxX;
+                }
+                else
+                {
+                    switch (PlayerDirection)
+                    {
+                        case Direction.Left:
+                            playerX += 1;
+                            break;
+                        case Direction.Right:
+                            playerX -= 1;
+                            break;
+                    }
+                }
+
+                if (0 <= newBoxY && newBoxY < Console.BufferHeight)
+                {
+                    boxY = newBoxY;
+                }
+                else
+                {
+                    switch(PlayerDirection)
+                    {
+                        case Direction.Up:
+                            playerY += 1;
+                            break;
+                        case Direction.Down:
+                            playerY -= 1;
+                            break;
+                    }
+                }
+                if (boxX == goalX && boxY == goalY)
+                {
+                    break;
+                }
+                
             }
+            Console.Clear();
+            Console.WriteLine("축하합니다 클리어입니다.");
+
+            Console.ResetColor();
         } 
     }
 }
