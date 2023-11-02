b400f3f268fa276628af55278807b34204110ba1
김세현 <t990421@naver.com>
2023-11-02 09:10:12 +0900

    Create Sokoban Program

--- KimSehyun/Sokoban-KimSehyun/ConsoleApp1/Program.cs
+++ KimSehyun/Sokoban-KimSehyun/ConsoleApp1/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace Sokoban
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.ResetColor();
+            Console.CursorVisible = false;
+            Console.Title = "I am 믿습니다.";
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            int playerX = 10;
+            int playerY = 20;
+
+            while (true)
+            {
+                /// Render
+                Console.Clear();
+                Console.CursorVisible = false;
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write("P");
+
+                /// ProcessInput
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                /// 처음
+                if (keyInfo.Modifiers == ConsoleModifiers.Shift)
+                {
+                    if (keyInfo.Key == ConsoleKey.LeftArrow && playerX > 2)
+                    {
+                        playerX -= 2;
+                    }
+                    if (keyInfo.Key == ConsoleKey.RightArrow && playerX + 3 < Console.BufferWidth)
+                    {
+                        playerX += 2;
+                    }
+                    if (keyInfo.Key == ConsoleKey.UpArrow && playerY > 2)
+                    {
+                        playerY -= 2;
+                    }
+                    if (keyInfo.Key == ConsoleKey.DownArrow && playerY + 3 < Console.BufferHeight)
+                    {
+                        playerY += 2;
+                    }
+                }
+                else
+                {
+                    if (keyInfo.Key == ConsoleKey.LeftArrow && playerX > 0)
+                    {
+                        playerX -= 1;
+                    }
+                    if (keyInfo.Key == ConsoleKey.RightArrow && playerX + 1 < Console.BufferWidth)
+                    {
+                        playerX += 1;
+                    }
+                    if (keyInfo.Key == ConsoleKey.UpArrow && playerY > 0)
+                    {
+                        playerY -= 1;
+                    }
+                    if (keyInfo.Key == ConsoleKey.DownArrow && playerY + 1 < Console.BufferHeight)
+                    {
+                        playerY += 1;
+                    }
+                }
+                /// 끝
+            }
         }
     }
 }
