05fa00982137b68aecd6afdc3d69570020e0947d
smchoi <seonmun.choi@gmail.com>
2023-11-02 10:28:37 +0900

    Implement player's movement

--- smchoi/Sokoban-smchoi/KimSeHyun/Program.cs
+++ smchoi/Sokoban-smchoi/KimSeHyun/Program.cs
@@ -4,7 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ResetColor();
+            Console.CursorVisible = false;
+            Console.Title = "I am 믿습니다.";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Clear();
+
+            int playerX = 10;
+            int playerY = 20;
+            
+            while (true)
+            {
+                //// ------------------------------ Render ---------------------------
+                Console.Clear();              
+                Console.CursorVisible = false;
+
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write("P");
+
+                // ------------------------------ ProcessInput ---------------------------
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                // ------------------------------ Update ---------------------------
+                int newPlayerX = playerX;
+                int newPlayerY = playerY;
+
+                if (keyInfo.Key == ConsoleKey.LeftArrow)
+                {
+                    newPlayerX -= 1;
+                }
+
+                if (keyInfo.Key == ConsoleKey.RightArrow)
+                {
+                    newPlayerX += 1;
+                }
+
+                if (keyInfo.Key == ConsoleKey.UpArrow)
+                {
+                    newPlayerY -= 1;
+                }
+
+                if (keyInfo.Key == ConsoleKey.DownArrow)
+                {
+                    newPlayerY += 1;
+                }
+
+                if (0 <= newPlayerX && newPlayerX < Console.BufferWidth)
+                {
+                    playerX = newPlayerX;
+                }
+
+                if (0 <= newPlayerY && newPlayerY < Console.BufferHeight)
+                {
+                    playerY = newPlayerY;
+                }
+
+
+            }
         }
     }
 }
