0eea95581dcf4da586e64c71f28602f061f23482
DINO <ekgmls9617@gmail.com>
2023-11-03 09:19:24 +0900

    refactor: Modify wall, box and goal system.
    - Increase number of box, wall and goal objects.
    - Clean up codes about objects.

--- dino/Sokoban-dino/projectSokoban/Program.cs
+++ dino/Sokoban-dino/projectSokoban/Program.cs
@@ -23,24 +23,21 @@ class Program
         Console.Clear();
 
         //player 위치
-        int[] playerPos = { 0, 0 };
-        int[,] portalPos = { { 10, 10 }, { 4, 6 }, { 17, 2 } };
-        int[,] transPos = { { 27, 13 }, { 20, 6 }, { 5, 15 } };
-        string[] directionCode = { " ", "←", "→", "↑", "↓" };
+        int[] playerPosition = { 0, 0 };
         Direction playerDirection = Direction.None; //0: None, 1: Left, 2: Right, 3: Up, 4: Down
                                                     //enumerate로 정리하면 깔끔해짐
 
 
         //박스의 기능: 플레이어와 충돌했을 때, 플레이어가 이동한 방향으로 박스도 한 칸 이동한다.
         //우리가 알고 있는 데이터: 플레이어 좌표, 박스 좌표
-        int boxX = 7;
-        int boxY = 7;
+        int[] boxPositionX = { 7 };
+        int[] boxPositionY = { 7 };
 
-        int wallX = 8;
-        int wallY = 8;
+        int[] wallPositionX = { 1, 2, 3, 4, 5 };
+        int[] wallPositionY = { 5, 5, 5, 5, 5 };
 
-        int goalX = 12;
-        int goalY = 12;
+        int[] goalPositionX = { 12, 13, 14 };
+        int[] goalPositionY = { 12, 13, 14 };
 
         //int flag = 0;
 
@@ -50,17 +47,24 @@ class Program
             Console.Clear();
             Console.CursorVisible = false;
 
-            Console.SetCursorPosition(playerPos[0], playerPos[1]);
+            Console.SetCursorPosition(playerPosition[0], playerPosition[1]);
             Console.Write("◆");
 
-            Console.SetCursorPosition(boxX, boxY);
+
+            for (int i = 0; i < wallPositionX.Length; i++)
+            {
+                Console.SetCursorPosition(wallPositionX[i], wallPositionY[i]);
+                Console.Write("*");
+            }
+
+            for (int i = 0; i < goalPositionX.Length; i++)
+            {
+                Console.SetCursorPosition(goalPositionX[i], goalPositionY[i]);
+                Console.Write("△");
+            }
+
+            Console.SetCursorPosition(boxPositionX[0], boxPositionY[0]);
             Console.Write("▲");
-
-            Console.SetCursorPosition(wallX, wallY);
-            Console.Write("○");
-
-            Console.SetCursorPosition(goalX, goalY);
-            Console.Write("△");
 
 
             //Process Input
@@ -70,14 +74,13 @@ class Program
             ConsoleModifiers modifiers = keyInfo.Modifiers;
 
             //Update
-            int newPlayerX = playerPos[0];
-            int newPlayerY = playerPos[1];
+            int newPlayerX = playerPosition[0];
+            int newPlayerY = playerPosition[1];
 
             //box
             //플레이어와 박스가 충돌했을 때, 박스는 플레이어가 이동한 방향으로 한 칸 이동한다.
-            int newBoxX = boxX;
-            int newBoxY = boxY;
-
+            int newBoxX = boxPositionX[0];
+            int newBoxY = boxPositionY[0];
 
             if (key == ConsoleKey.A || key == ConsoleKey.LeftArrow)
             {
@@ -124,52 +127,56 @@ class Program
                         Console.WriteLine($"Wrong direction data!\nPlayer Direction: {playerDirection}");
                         Environment.Exit(1);
                         break;
-
-                        ;
                 }
             }
 
 
             //벽 기능: 벽의 위치에는 어떤 오브젝트도 위치할 수 없다. => (벽의 좌표) != (박스의 좌표) && (벽의 좌표) != (플레이어 좌표)
-            if (wallX == newPlayerX && wallY == newPlayerY || wallX == newBoxX && wallY == newBoxY)
+            bool isCollidedToWall = false;
+            for (int i = 0; i < wallPositionX.Length; i++)
+            {
+                if (wallPositionX[i] == newPlayerX && wallPositionY[i] == newPlayerY || wallPositionX[i] == newBoxX && wallPositionY[i] == newBoxY)
+                {
+                    isCollidedToWall = true;
+                    break;
+                }
+
+            }
+
+            if (isCollidedToWall)
                 continue;
 
 
             if (0 <= newPlayerX && newPlayerX < Console.BufferWidth)
             {
-                playerPos[0] = newPlayerX;
+                playerPosition[0] = newPlayerX;
             }
 
             if (0 <= newPlayerY && newPlayerY < Console.BufferHeight)
             {
-                playerPos[1] = newPlayerY;
+                playerPosition[1] = newPlayerY;
             }
+
+            //if (wallX == newBoxX && wallY == newBoxY)
+            //continue;
 
             if (0 <= newBoxX && newBoxX < Console.BufferWidth)
             {
-                boxX = newBoxX;
+                boxPositionX[0] = newBoxX;
             }
 
             if (0 <= newBoxY && newBoxY < Console.BufferHeight)
             {
-                boxY = newBoxY;
+                boxPositionY[0] = newBoxY;
             }
 
-
-            if (boxX == goalX && boxY == goalY)
-            {
-                Thread.Sleep(150);
-                Console.SetCursorPosition(15, 15);
-                Console.WriteLine("Congratulation! You clear this stage!");
-                break;
-            }
 
             if (key == ConsoleKey.R)
             {
-                playerPos[0] = 0;
-                playerPos[1] = 0;
-                boxX = 7;
-                boxY = 7;
+                playerPosition[0] = 0;
+                playerPosition[1] = 0;
+                boxPositionX[0] = 7;
+                boxPositionY[0] = 7;
             }
 
             if (key == ConsoleKey.Escape)
